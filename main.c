22e421253add5ce3d66480829478ee574f177b96
miji <mijilee0413@sookmyung.ac.kr>
2023-11-19 13:11:40 +0900

    11-3(2)

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-//실습3-1
-//call by value
+//실습3-2
+//call by reference
 
-void swap (int x, int y) {
+void swap (int *x, int *y){
 	int temp;
-	temp = x;
-	x = y;
-	y = temp;
+	
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
-void main(void) {
+int main(void){
 	int a = 3;
 	int b = 5;
-	swap(a, b);
+	swap(&a, &b);   //'swap(&a, &b);는 주소에 의한 참조를 사용하여 a와 b의 값을 서로 교환하는 함수 호출
 	printf("a:%i, b:%i\n", a, b);
+	
 	return 0;
 }
