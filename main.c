6e7b8ad5bfbbccd041bf96a331356fa804763c96
miji <mijilee0413@sookmyung.ac.kr>
2023-11-19 11:17:36 +0900

    11-1

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-//실습2
+//실습1
 
-void main(void){
-	int i = 4; 
-	
-	int *pi = &i;
-	char *pc = &i;
-	printf("%i, %i, %i\n", i, *pi, *pc);
+int main(void) {
+	int i = 10;
+	char c = 'a';
+
+	int* iptr;
+	char* cptr;
+	int* iptr2;
+
+	iptr = &i;
+	cptr = &c;
+	iptr2 = iptr;
+
+	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p %i\n", iptr2, *iptr2);
 
 	return 0;
 }
-
